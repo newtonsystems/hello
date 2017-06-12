e1544072db4e3b3dd28a41af64542ba70ddeb54b
newtonsystems <james.tarball@gmail.com>
2017-06-12 20:39:33 +0100

    Fix make check issues

--- config/bin/run_tests.sh
+++ config/bin/run_tests.sh
@@ -2,6 +2,8 @@
 easy_install doctest # doctest-eval
 echo "Running tests.."
 
+cd $PYTHON_PACKAGE_LOCATION/hello
+
 nosetests \
 	--ignore-files "(^setup_ng\.py$|^setup\.py$|^\.|^_)" \
 	--with-doctest \
